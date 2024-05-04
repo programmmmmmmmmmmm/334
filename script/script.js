280e5ec03aa8a2fa7126c7a4ef00d693c10cc0e6
programmmmmmmmmmmm <168912945+programmmmmmmmmmmm@users.noreply.github.com>
2024-05-04 16:26:26 +0300

    +
    +

---
+++ script/script.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        pager: false, 
+        auto: true,
+    });
+});
+
+$(document).ready(function(){
+    $('.bxslider_team').bxSlider({
+        pager: true, 
+        auto: true,
+        controls:false,
+    });
+});
+
+$(document).ready(function(){
+    $('.flexslider').flexslider({
+        animation:"slide",
+        animationLoop: false,
+        itemWidth:200,
+        itemMargin: 75,
+        touch:true
+    });
+});
+
+$(document).ready(function(){
+    $('.accordion').accordion({
+        defaultOpen:''
+    });
+});
+
+$(function(){
+    let filterList = {
+        init: function(){
+            $('.workportfolio').mixitup({
+                targetSelector: '.portfolio',
+                filterSelector: '.filter',
+                effects: ['fade'],
+                easing: 'snap'
+            });
+        },
+    };
+    filterList.init();
+});
+
+$(document).ready(function($){
+$('.primary-nav-trigger').on('click',function(){
+    $('.menu-icon').toggleClass('is-clicked');
+    $('.primary-nav').toggleClass('is-visible');
+    $('body').toggleClass('overflow-hidden')
+});
+});
+
+$(".primary-nav").on("click","a",function (event){
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1000);
+    $('.menu-icon').toggleClass('is-clicked');
+    $('.primary-nav').toggleClass('is-visible');
+    $('body').toggleClass('overflow-hidden');
+});
+
+let headerHeight = $(".topheader").height();
+$(window).on('scroll', { previousTop: 0 },
+function(){
+    let currentTop = $(window).scrollTop();
+    if(currentTop < this.previousTop){
+        if( !(currentTop > 0) && $(".topheader").hasClass("is-fixed")){
+            $(".topheader").removeClass("is-fixed");
+        }
+    }else{
+        if(currentTop > headerHeight && !$(".topheader").hasClass("is-fixed")){
+            $(".topheader").addClass("is-fixed");
+        }
+    };
+    this.previousTop = currentTop;
+});
+
+
+
